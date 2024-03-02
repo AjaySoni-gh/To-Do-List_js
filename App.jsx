94c0bce568ff02d096192fd86957abf877e8e60d
Ajay soni <144725314+AjaySoni-gh@users.noreply.github.com>
2024-03-01 23:18:49 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'; // Import useState from react
+import ReactDOM from 'react-dom'; // Import ReactDOM
+import './index.css'; // Import your CSS file
+import ToDoList from './ToDoList';
+
+function App() {
+  return (
+    <>
+      <ToDoList />
+    </>
+  );
+}
+
+export default App; // Export the App component
